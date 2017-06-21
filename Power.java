bde6b986d8560127ed62b284d12673eb0302652c
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-06-21 14:15:31 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,18 @@
+package aaaa;
+
+public class Power {
+
+	public static void main(String[] args) 
+	{
+		int b=2;
+		int e=3;
+		int res=1;
+		while(e!=0)
+		{
+			res=res*b;
+			e--;
+		}
+		System.out.println(" "+res);
+	}
+
+}
