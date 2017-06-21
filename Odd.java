364173fadd337bbafbb5849175e71f3f12736a40
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-06-21 14:21:16 +0530

    Add files via upload

---
+++ Odd.java
@@ -0,0 +1,19 @@
+package aaaa;
+
+public class Odd {
+
+	public static void main(String[] args)
+	{
+		int i;
+		for(i=1;i<=10;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.print(" "+i);
+			}
+		}
+		
+
+	}
+
+}
