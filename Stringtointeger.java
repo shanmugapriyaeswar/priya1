22e936794e363de87f50bcc75c9043cd57ef8fae
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-06-29 14:16:05 +0530

    Add files via upload

---
+++ Stringtointeger.java
@@ -0,0 +1,13 @@
+package yyy;
+
+public class Stringtointeger {
+
+	public static void main(String[] args)
+	{
+		String number="100";
+		int result=Integer.valueOf(number);
+		System.out.println(result);
+
+	}
+
+}
