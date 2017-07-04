4415af6a849f5e45ac3874b017175c349b045198
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-07-04 12:02:14 +0530

    Add files via upload

---
+++ Countcharacter.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.util.Scanner;
+
+public class Countcharacter {
+
+	public static void main(String[] args)
+	{
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+		int len=str.length();
+		System.out.println(+len);
+		
+
+	}
+
+}
