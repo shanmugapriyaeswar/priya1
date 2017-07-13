df1a717da097e819f8e9172dbfc33d097e236cc4
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-07-13 15:20:39 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,19 @@
+package code;
+import java.util.*;
+public class Bitwise {
+
+	public static void main(String[] args)
+	{
+		int a,b;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		a=a^b;
+		b=a^b;
+		a=a^b;
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
