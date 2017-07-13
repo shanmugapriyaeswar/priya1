5a6a86a9e4f44cae23a30b88761d2f1206af30ab
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-07-13 15:13:30 +0530

    Add files via upload

---
+++ Swaping.java
@@ -0,0 +1,22 @@
+package code;
+import java.util.*;
+public class Swaping {
+
+	public static void main(String[] args)
+	{
+		int a;
+		int b;
+		int temp;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		b=s.nextInt();
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+
+}
